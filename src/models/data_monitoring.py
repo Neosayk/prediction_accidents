7a5b3e0ce86e345f94043d19accb5da66c6d716c
Neosayk <50923884+Neosayk@users.noreply.github.com>
2023-05-15 03:39:20 -0400

    Add files via upload

--- src/models/data_monitoring.py
+++ src/models/data_monitoring.py
@@ -2,10 +2,12 @@ import os
 import tarfile
 import fnmatch
 import glob
+import time
 from github import Github
 
 
 def make_tarfile(file_extensions):
+    timestamp = time.strftime('%Y%m%d_%H%M%S_')
     target_dir = os.getcwd()
 
     for file_extension in file_extensions:
@@ -14,7 +16,7 @@ def make_tarfile(file_extensions):
         for filename in files_to_archive:
             print(f"Commencer la création de l'archive pour {filename}...")
             
-            archive_name = os.path.join(target_dir, os.path.basename(filename) + '.tar.gz')
+            archive_name = os.path.join(target_dir, timestamp + os.path.basename(filename) + '.tar.gz')
             print(f"Le nom de l'archive sera {archive_name}")
             
             with tarfile.open(archive_name, "w:gz") as tar:
@@ -23,35 +25,35 @@ def make_tarfile(file_extensions):
             print(f"Archive {archive_name} créée avec succès.")
 
 
-def upload_to_github(file_types, token, repo_name, commit_message, branch, file_path_base):
-    if isinstance(file_types, str):
-        file_types = [file_types]
+def upload_to_github(paths, token, repo_name, commit_message, branch, file_path_base):
+    if isinstance(paths, str):
+        paths = [paths]
     print("Préparation pour l'envoi à Github...")
     g = Github(token)
     repo = g.get_user().get_repo(repo_name)
 
     input_path = os.getcwd()
 
-    for file_type in file_types:
-        if os.path.isdir(file_type):
-            dir_to_walk = os.path.join(input_path, file_type)
+    for path in paths:
+        file_pattern = None
+        if os.path.isdir(path):
+            dir_to_walk = os.path.join(input_path, path)
         else:
-            dir_to_walk = input_path
+            dir_to_walk, file_pattern = os.path.split(path)
+            if not dir_to_walk:
+                dir_to_walk = input_path
 
         for dirpath, dirnames, filenames in os.walk(dir_to_walk):
             print(f"Dossier actuel : '{dirpath}', sous-dossiers : {dirnames}")
-            if not os.path.isdir(file_type):
-                filenames = fnmatch.filter(filenames, file_type)
+            if file_pattern:
+                filenames = fnmatch.filter(filenames, file_pattern)
             for filename in filenames:
                 local_file_path = os.path.join(dirpath, filename)
-                rel_path = os.path.relpath(local_file_path, input_path).replace('\\', '/')
+                rel_path = os.path.relpath(local_file_path, dir_to_walk).replace('\\', '/')
                 
-                # Ignorer les fichiers du dossier "./mlruns/0"
                 if rel_path.startswith("mlruns/0"):
                     continue
 
-                if file_type == "mlruns" and rel_path.startswith("mlruns/"):
-                    rel_path = rel_path[len("mlruns/"):]
                 github_file_path = f"{file_path_base}/{rel_path}"
                 with open(local_file_path, "rb") as file:
                     content = file.read()
@@ -65,14 +67,3 @@ def upload_to_github(file_types, token, repo_name, commit_message, branch, file_
                 except Exception as e:
                     print(f"Erreur lors de la création du fichier '{github_file_path}' sur Github : {str(e)}")
     print(f"Les fichiers ont été téléchargés avec succès sur le dépôt Github '{repo_name}'")
-
-
-def delete_files(file_extensions):
-    for file_extension in file_extensions:
-        files_to_delete = glob.glob(f"*{file_extension}")
-        for filename in files_to_delete:
-            try:
-                os.remove(filename)
-                print(f"Fichier {filename} supprimé avec succès.")
-            except Exception as e:
-                print(f"Erreur lors de la suppression du fichier {filename} : {str(e)}")
