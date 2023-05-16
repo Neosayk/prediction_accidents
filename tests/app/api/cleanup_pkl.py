ab4d603a3d8ee26cabf53226c54872b0a38fabcf
Neosayk <50923884+Neosayk@users.noreply.github.com>
2023-05-15 21:47:21 -0400

    Delete cleanup_pkl.py

--- tests/app/api/cleanup_pkl.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-
-target_dir = Path("src/app/api/")
-pkl_files = list(target_dir.glob("*.pkl"))
-pkl_files.sort(key=os.path.getmtime)
-if len(pkl_files) > 2:
-    for file in pkl_files[:-2]:
-        file.unlink()
